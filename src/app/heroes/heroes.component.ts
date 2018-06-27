d574c61bc3483a6150c3701393cda0f96c777601
vicdata <vghaya@gmail.com>
2018-06-27 17:24:39 +0200

    Navigation to hero details / Routable HeroDetailComponent

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,24 +8,16 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
   heroes: Hero[];
-
-  selectedHero: Hero;
 
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
-  this.getHeroes();
-}
-
-getHeroes(): void {
-this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes);
-}
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
