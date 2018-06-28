2dcfe99f40eef24713d171b1b3a451b3f2a5c2c7
vicdata <vghaya@gmail.com>
2018-06-28 16:26:01 +0200

    Enable HTTP Services/ Simulate a data server

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,23 +3,33 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-    constructor(private messageService: MessageService) { }
+  constructor(
+  private http: HttpClient,
+  private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  private heroesUrl = 'api/heroes';  // URL to web api
+
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
   }
 
   getHero(id: number): Observable<Hero> {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add('HeroService: ' + message);
   }
 
 }
